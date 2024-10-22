1dc2308ab8d166ffe9681239da5d2cee74f508a4
Nico <trynx93@gmail.com>
2024-10-22 11:55:23 +0300

    feat: add report print of the document

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ def main():
     num_words = get_count_words(book_text)
     chars_dict = get_char_dict(book_text)
 
-    print(f"{num_words} words in the document!")
-    print(chars_dict)
+    print_report(file_path, num_words, chars_dict)
 
 def get_book_text(path):
     with open(path) as f:
@@ -28,6 +27,26 @@ def get_char_dict(text):
 
     return chars
 
+def sort_on(dict):
+    return dict["count"]
+
+def print_report(name, words_count, char_dict):
+    print(f"--- Begin report of {name} ---")
+    print(f"{words_count} words found in the document")
+    print()
+
+    char_list = []
+    for char, count in char_dict.items():
+        if not char.isalpha():
+            continue
+
+        char_list.append({"char": char, "count":count})
+    
+    char_list.sort(reverse=True, key=sort_on)
+    for char_obj in char_list:
+        print(f"The '{char_obj["char"]}' character was found {char_obj["count"]} times")
+
+    print("--- End report ---")
 
 
 main()
